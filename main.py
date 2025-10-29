755ccd92b80a15b7cc11d5bbf42a02baf45608c7
cwolfe1080 <cewolfe19@gmail.com>
2025-10-28 20:44:19 -0400

    Created title screen and play button

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import pyxel
 
-# Define the game class
+# All other functions
+
+def load(file):
+    pyxel.text(80, 100, "Loading...")
+    pyxel.load(file)
+
+# All Pyxel functions
 class Title:
     def __init__(self):
-        pass      
+        self.touchingPlay = False
 
     def update(self):
-        if pyxel.btnp(pyxel.KEY_ESCAPE):
-            pyxel.quit()
+        self.mx = pyxel.mouse_x
+        self.my = pyxel.mouse_y
+        if self.mx > 42 and self.mx < 115 and self.my > 40 and self.my < 68:
+            self.touchingPlay = True
+        else:
+            self.touchingPlay = False
         
 
     def draw(self):
-        pyxel.text(50, 50, "chime", 9)
+        pyxel.blt(60, 15, 0, 2, 0, 38, 8, None, None, 3)
+        if self.touchingPlay:
+            pyxel.blt(67, 50, 0, 24, 17, 24, 9, None, None, 3)
+        else:    
+            pyxel.blt(67, 50, 0, 0, 17, 24, 9, None, None, 3) # x is at 67 :D
 
-# Start the game
 class Game:
     def __init__(self):
         pyxel.init(160, 120, "chime")  
@@ -22,6 +35,7 @@ class Game:
 
         self.level = 0 # Initilize level
         self.title = Title() # Start title screen
+        pyxel.mouse(True) # Show mouse cursor
 
     def update(self):
         if self.level == 0:
@@ -36,7 +50,7 @@ class Game:
         if self.level == 0:
             self.title.draw()
         elif self.level == 1:
-            pyxel.text(80, 100, "Loading...")
+            pass # Load file here
             
 game = Game()
 pyxel.run(game.update, game.draw)
